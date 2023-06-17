fc4cf615aadbcdfb4e3f07eeaa55dca37e7ec50c
fbelam <36649120+fbelam@users.noreply.github.com>
2023-06-17 20:17:17 -0300

    Primeiro mini projeto
    Conhecendo o Flask

---
+++ Estudo_api/Flask_api/bd.py
@@ -0,0 +1,26 @@
+Carros = [
+    {
+        'id': 1,
+        'marca': 'fiat',
+        'modelo': 'marea',
+        'ano': 1999
+    },
+    {
+        'id': 2,
+        'marca': 'mercedes',
+        'modelo': '180',
+        'ano': 2020
+    },    
+    {
+        'id': 3,
+        'marca': 'volks',
+        'modelo': 'gol',
+        'ano': 2003
+    },    
+    {
+        'id': 4,
+        'marca': 'bmw',
+        'modelo': 'x5',
+        'ano': 2023
+    },
+]
---
+++ Estudo_api/Flask_api/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, make_response,jsonify, request
+from bd import Carros
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/carros', methods=['GET'])
+def get_carros():
+    return make_response(
+        jsonify(
+            mensagem='Lista de Carros.',
+            dados=Carros
+            )
+    )
+
+@app.route('/carros', methods=['POST'])
+def create_carro():
+    carro = request.json
+    Carros.append(carro)
+    return carro
+    
+app.run()
+
